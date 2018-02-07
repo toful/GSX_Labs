9c8bb7a5fee0bbc4400b925987a100dfcfdab6d0
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-02-07 21:16:05 +0100

    Afegits scripts gpgp.sh i rpgp.sh

--- Prac1/gpgp.sh
+++ Prac1/gpgp.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+#Authors:	Crstòfol Daudén Esmel i Aleix Mariné Tena
+#Date:		07-02-2018
+#Version:	1.0
+#Description:	Retorna el propietari, grup i permisos dels fitxers el path dels quals és troba en el fitxer d'entrada
+#Arg 1:		Fitxer amb els paths d'altres fitxers
+
+
+if [ $# -lt 1 ]
+then 
+	echo ERROR, no file detected >&2
+	exit 1
+fi
+
+IFS=$'\n'
+for file in $(cat $1)
+do
+	echo $file $(stat -c "%U %G %a" $file)
+done
+exit 0
---
+++ Prac1/rpgp.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#Authors:	Crstòfol Daudén Esmel i Aleix Mariné Tena
+#Date:		07-02-2018
+#Version:	1.0
+#Description:	Mira si s'ha canviat algún dels atributs del fitxers descrits en l'argument d'entrada i dona la possibilitat de tornar al seu estat inicial
+#Arg 1:		Fitxer amb el nom(path absolut), propietari, grup i permisos de X fitxers
+
+if [ $# -lt 1 ]
+then
+	echo ERROR, no file as an argument >&2
+	exit 1
+fi
+
+IFS=$'\n'
+for file in $(cat $1)
+do
+	path=$(echo $file | cut -d ' ' -f1)
+
+	#Comprovo si el fitxer existeix
+	if [ -e $path ]
+	then
+		#si existeix comprovo si s'ha modificat algún dels paràmetres i li dino a l'usuari la possibilitat de restaurar-los
+		user=$(echo $file | cut -d ' ' -f2)
+		group=$(echo $file | cut -d ' ' -f3)
+		perm=$(echo $file | cut -d ' ' -f4)
+		echo $path $(stat -c "%U %G %a" $path)
+
+		if [ $(stat -c "%a" $path) != $perm ]
+		then
+			echo S'ha detectat que s'han modificat els permisos, els vols tornar al seu estat anterior: $perm?"(S/N)"
+			read r
+			if [ r='S' -o r='s' ]
+			then
+				chmod $perm $path
+				echo "S'han modificat els permisos a:" $perm
+			fi
+		fi
+
+		if [ $(stat -c "%U" $path) != $user ]
+		then
+			echo S'ha detectat que s'ha modificat el propietari, el vols tornar al seu estat anterior: $user"(S/N)"
+			read r
+			if [ r='S' -o r='s' ]
+			then
+				chown $user $path
+				echo "S'ha modificat el propietari a:" $user
+			fi
+		fi
+
+		if [ $(stat -c "%G" $path) != $group ]
+		then
+			echo S'ha detectat que s'han modificat el grup, el vols tornar al seu estat anterior: $group?"(S/N)"
+			read r
+			if [ r='S' -o r='s' ]
+			then
+				chgrp $group $path
+				echo "S'ha modificat el grup a:" $group
+			fi
+		fi
+
+	else
+		echo ERROR, el fitxer $path ja no existeix
+	fi
+done
+exit 0
