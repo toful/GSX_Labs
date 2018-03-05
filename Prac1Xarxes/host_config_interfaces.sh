3fc8b05fc226008d391192aea3709f6f3ec97bb0
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-03-05 20:46:24 +0100

    Petit canvi en els fitxers de xarxes

--- Prac1Xarxes/host_config_interfaces.sh
+++ Prac1Xarxes/host_config_interfaces.sh
@@ -26,8 +26,11 @@ config="auto lo\niface lo inet loopback
 	\n
 	\nallow-hotplug $i1\niface $i1 inet static\n\taddress 192.168.17.2\n\tnetmask 255.255.255.0\n\tnetwork 192.168.17.0\n\tbroadcast 192.168.17.255"
 
+#escribim canvis en el fitxer /etc/network/interfaces
+echo -e $config > /etc/network/interfaces
+
 #Afegim les rutes que calguin
-echo "up ip route add 192.168.16.0/24 via 192.168.17.1/24 dev $1 >> /etc/network/interfaces"
+echo "up ip route add 192.168.16.0/24 via 192.168.17.1/24 dev $1" >> /etc/network/interfaces
 
 #ip route add default via <ip router extern>? No fa falta no? en teoria el router ja té conexió
 
