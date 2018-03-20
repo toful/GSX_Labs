36d7b00888507609b79e0fb6f44d5c461bc9ac65
jmarinllao <josepmarinllao@gmail.com>
2018-03-20 09:31:40 +0100

    comments

--- Prac1/canviagroup.sh
+++ Prac1/canviagroup.sh
@@ -29,7 +29,7 @@ if [ "$1" = "-h" ]; then
 	exit 0
 fi
 
-#mirem que s'hagin introduit tots els arguments
+#mirem que s'hagin introduit l’arguments
 if [ $# -lt 1 ] 
 then
 	echo "ERROR, you have to indroduce at least one argumnet" >&2
@@ -37,25 +37,31 @@ then
 	exit 1
 fi
 
+#obtenim el grup primari actual, per despres recuperar-lo.
 primary_group=$(id -gn)
+#obtenim la llista de tots els grups
 groups=($(groups))
 
+#es comprova que es tracti d’un grup correcte.
 if [[ " ${groups[*]} " != *" $1 "* ]]; then
 	echo "ERROR, you have indroduced an invalid group name. It must be one of the secondary groups:" >&2
 	echo ${groups[@]/$primary_group}
 	exit 1
 fi
 
+#si el grup que s’ha introduït es el primari, també es notifica.
 if [[ " $primary_group " = *" $1 "* ]]; then
 	echo "ERROR, you have indroduced the current primary group. It must be one of the secondary groups:" >&2
 	echo ${groups[@]/$primary_group}
 	exit 1
 fi
 
+#es crida la comanda de canviar el grup.
 newgrp $1
 
 reset_group () {
 	newgrp $primary_group
 }
 
+#revertir al grup primari incial en el cas que es comandi exit
 trap "reset_group" EXIT
