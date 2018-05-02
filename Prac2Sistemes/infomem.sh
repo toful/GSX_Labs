cf5be3f4024c0f0ab2b67cf8ead17147097023ce
AleixMT <aleixaretra@gmail.com>
2018-05-02 20:54:01 +0200

    infomem finished fuck ubuntu systems

--- Prac2Sistemes/infomem.sh
+++ Prac2Sistemes/infomem.sh
@@ -10,17 +10,18 @@ INSTRUCCIONS
 # Autors: Cristòfol Daudèn, Aleix Marine i Josep Marín Llaó                                           
 # Data d'implementació: 2/5/2018                                                   5
 # Versio 1.0                                                                        
-# Permisos: permisos per a crear un fitxer a $HOME                                   
+# Permisos: Per a que la comanda du pugui comptabilitzar tots els fitxers ha de poder 
+# accedir a tots els subdirectoris que pengen de $HOME.                                  
 # Descripció i paràmetres: Es vol implementar un script que s'executi en el moment en que un
 # usuari entra al sistema i l'informi de l'espai de disc que s'està utilitzant sota del seu   
 # directori d'entrada. Volem usar la comanda du per a calcular l'espai del disc que ocupa.
 #
-# Aquest script no necessita arguments, excepte el d'ajuda que es opcional.
-# 
-# Aquest script instal·la una sola línia de codi al fitxer ~/.profile que 
-# mostra una notificació d'escriptori quan l'usuari es logueja.
+# Arguments: Sense arguments. Tampoc necessita ajuda ja que no esta pensat per 
+# a ser executat a través d'un tty. 
 ###############################################################################
 
 
-notify-send "Avís memoria" "Hola $USER, la teva carpeta d'inici ocupa $(du -s $HOME --exclude=*/.* | cut -d$'\t' -f1) Bytes ($(du -sh $HOME --exclude=*/.* | cut -d$'\t' -f1))"
+xmessage -buttons Ok:0 -default Ok -nearmouse "Hello $USER, your home folder weights $(du -s $HOME | cut -d$'\t' -f1) Bytes ($(du -sh $HOME | cut -d$'\t' -f1))" -timeout 30
+
+#notify-send "Avís memoria" "Hola $USER, la teva carpeta d'inici ocupa $(du -s $HOME --exclude=*/.* | cut -d$'\t' -f1) Bytes ($(du -sh $HOME --exclude=*/.* | cut -d$'\t' -f1))"
 
