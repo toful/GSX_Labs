c6bef4d169916ad274d926c27aee434903ca5014
milax <milax@sense-nom.lab.deim>
2018-05-11 17:06:04 +0200

    ssh script added

---
+++ Prac2Xarxes/inici_ssh.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+function display_help(){
+	echo -e "This script configurates ssh service in a indicated machine. All machines
+will install and configure openssh-server. 
+	Argument one: [server|router|client]
+	"
+}
+
+function router_config(){
+	
+}
+
+function client_config()
+{
+	apt-get install openssh-client
+}
+
+function server_config(){
+	
+}
+
+while getopts :h option
+do
+	case "$option" in
+    	h)
+      		display_help
+      		exit 1
+      	;;
+    	*)
+      		echo "ERROR: Invalid parameters" >&2 
+      		display_help
+      		exit 1
+      ;;
+  esac
+done
+
+apt-get install openssh-server
+
+case "$1" in
+	router)
+		router_config
+		exit 0
+	   	;;
+	server)
+		server_config
+		exit 0
+		;;
+	client)
+		client_config
+		exit 0
+		;;
+	*)
+		echo -e "ERROR: machine indication needed:\n\trouter\n\tserver\n\tclient"
+		exit 1
+esac
+
