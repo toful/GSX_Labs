3c6ab16aad7b12de8b4377feda45d6f1fd8fc6ed
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-05-22 15:58:08 +0200

    Toful: configuring apache2

---
+++ Prac2Xarxes/apache.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir /var/www/html/taller
+mkdir /var/www/html/tenda
+cp html_files/index.html /var/www/html/index.html
+cp html_files/taller.html /var/www/html/taller/taller.html
+cp html_files/tenda.html /var/www/html/tenda/tenda.html
+
+cp html_files/taller.conf /etc/apache2/sites-available/taller.conf
+cp html_files/tenda.conf /etc/apache2/sites-available/tenda.conf
+
+
+
