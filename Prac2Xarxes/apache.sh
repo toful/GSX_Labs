c7e3ae80ba33466d172159d56dcf307f6b9b774c
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-05-25 01:09:42 +0200

    added recursion to DMZ DNS

--- Prac2Xarxes/apache.sh
+++ Prac2Xarxes/apache.sh
@@ -9,6 +9,9 @@ cp html_files/tenda.html /var/www/html/tenda/index.html
 cp html_files/taller.conf /etc/apache2/sites-available/taller.conf
 cp html_files/tenda.conf /etc/apache2/sites-available/tenda.conf
 
+cd /etc/apache2/sites-available
+a2ensite taller.conf
+a2ensite tenda.conf
 
 service apache2 restart
 
